b4e37ff16ca7304e21db753ec9575b52a4e037cc
iamsahilshukla <iamsahilshukla@gmail.com>
2023-04-24 20:29:06 +0530

    added logic for redundant news

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://hacker-news.firebaseio.com/v0";
 const topStoriesUrl = `${baseUrl}/topstories.json`;
 const bestStoriesUrl = `${baseUrl}/beststories.json`;
+const newStoriesUrl = `${baseUrl}/newstories.json`;
 
 const renderNewsItem = (story) => {
     const newsElement = document.createElement('div');
@@ -14,25 +15,31 @@ const renderNewsItem = (story) => {
 
 const newsContainer = document.querySelector('#news-container');
 
-let random = (max) => {
-    return Math.floor(Math.random() * max);
-}
-
-let notVisitAgain = 0;
+const addNewsItem = (() => {
+    const addedNewsIds = {};     //storing unique ids
+    return (story) => {
+        if (!addedNewsIds[story.id]) {
+            addedNewsIds[story.id] = true;  // marking ids for stoping redundancy
+            const newsElement = renderNewsItem(story);
+            newsContainer.appendChild(newsElement);
+        }
+        else{
+            console.log("injin");
+        }
+    }
+})();
 
 fetch(topStoriesUrl)
     .then(response => response.json())
     .then(topStories => {
         for (let i = 0; i < 30; i++) {
-            notVisitAgain = topStories[i];
             fetch(`${baseUrl}/item/${topStories[i]}.json`)
                 .then(response => response.json())
                 .then(story => {
-                    const newsElement = renderNewsItem(story);
-                    newsContainer.appendChild(newsElement);
+                    addNewsItem(story);
                 })
                 .catch(err => {
-                    console.log(err)
+                    console.error(err);
                 });
         }
     });
@@ -41,18 +48,28 @@ fetch(bestStoriesUrl)
     .then(response => response.json())
     .then(bestStories => {
         for (let i = 0; i < 30; i++) {
-            if (notVisitAgain == bestStories[i]) {
-                console.log(bestStories[i]);
-                continue;
-            }
             fetch(`${baseUrl}/item/${bestStories[i]}.json`)
                 .then(response => response.json())
                 .then(story => {
-                    const newsElement = renderNewsItem(story);
-                    newsContainer.appendChild(newsElement);
+                    addNewsItem(story);
                 })
                 .catch(err => {
-                    console.log(err)
+                    console.error(err);
                 });
         }
     });
+
+fetch(newStoriesUrl)
+.then(response => response.json())
+.then(newStories => {
+    for (let i = 0; i < 30; i++) {
+        fetch(`${baseUrl}/item/${newStories[i]}.json`)
+            .then(response => response.json())
+            .then(story => {
+                addNewsItem(story);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+});
